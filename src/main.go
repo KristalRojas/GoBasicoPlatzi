c73c905182595c80d7cddd2acf6e107c92d29954
krojas <kristal.rojas@mercadolibre.cl>
2021-07-08 10:45:32 -0400

    Funciones y funciones anonimas

--- src/main.go
+++ src/main.go
@@ -2,87 +2,29 @@ package main
 
 import "fmt"
 
+func normalFunction(message string) {
+	fmt.Println(message)
+}
+
+func tripleArgument(a int, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int {
+	return a * 2
+}
+
+func dobleReturn(a int) (c, d int) {
+	return a, a * 2
+}
+
 func main() {
-	//DECLARACION DE CONSTANTES
-	const pi float64 = 3.14
-	const pi2 = 3.1415
-	fmt.Println("pi: ", pi)
-	fmt.Println("pi2: ", pi2)
+	normalFunction("Hola Mundo")
+	tripleArgument(1, 2, "hola")
 
-	//DECLARACION DE VARIABLES ENTERAS
-	base := 12
-	var altura int = 14
-	var area int
-	println("base: ", base)
-	println("altura: ", altura)
-	println("area: ", area)
+	value := returnValue(2)
+	fmt.Println("Value: ", value)
 
-	//ZERO VALUES
-	var a int
-	var b float64
-	var c string
-	var d bool
-
-	println(a, b, c, d)
-
-	//Area cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
-	println("Area cuadrado es: ", areaCuadrado)
-	x := 10
-	y := 50
-
-	//Suma
-	result := x + y
-	println("Suma: ", result)
-
-	//Resta
-	result = y - x
-	println("Resta: ", result)
-
-	//Multiplicacion
-	result = x * y
-	println("Multiplicacion: ", result)
-
-	//Division
-	result = y / x
-	println("Division: ", result)
-
-	//Modulo
-	result = y % x
-	println("Modulo: ", result)
-
-	// Incremental
-	x++
-	println("Incremental: ", x)
-
-	// Decremental
-	x--
-	println("Decremental: ", x)
-
-	//RETOS
-	//RECTANGULO, TRAPECIO Y DE UN CIRCULO
-
-	//Area Rectangulo
-	const largo = 2
-	const ancho = 4
-
-	resultArea := largo * ancho
-
-	println("Area Rectangulo: ", resultArea)
-
-	//Area Trapecio
-	baseBaja := 10
-	baseAlta := 4
-
-	resultArea = ((baseBaja + baseAlta) * altura) / 2
-	println("Area Trapecio: ", resultArea)
-
-	//Area Circulo
-	var radio = 5
-
-	var resultCircle float64
-	resultCircle = (pi * (float64(radio) * float64(radio)))
-
-	println("Area Circulo: ", resultCircle)
+	value1, value2 := dobleReturn(2)
+	fmt.Println("Value1: ", value1, "  | Value2: ", value2)
 }
