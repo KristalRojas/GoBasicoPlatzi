5b708247b7cad15312cc9c5f4c8bcf7ab428b990
krojas <kristal.rojas@mercadolibre.cl>
2021-07-08 14:40:08 -0400

    Switch Case sin condicion

--- src/main.go
+++ src/main.go
@@ -9,4 +9,15 @@ func main() {
 	default:
 		fmt.Println("Es impar")
 	}
+
+	//Switch case sin condicion
+	value := 200
+	switch {
+	case value > 100:
+		fmt.Println("Es mayor a 100")
+	case value < 0:
+		fmt.Println("Es menor a 0")
+	default:
+		fmt.Println("No condicion")
+	}
 }
