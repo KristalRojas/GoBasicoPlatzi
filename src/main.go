4769693a94b6557b74dcaa1895ab316caae4dd8b
krojas <kristal.rojas@mercadolibre.cl>
2021-07-08 14:29:07 -0400

    Operadores logicos y de comparacion

--- src/main.go
+++ src/main.go
@@ -2,23 +2,48 @@ package main
 
 import "fmt"
 
+func esPar(n int) string {
+	if n%2 == 0 {
+		return "Es Par"
+	}
+	return "Es Impar"
+}
+
+func loginAdmin(user, pass string) string {
+	correctName := "Admin"
+	correctPass := "AdminPass"
+	if correctName == user && correctPass == pass {
+		return "Logeado"
+		// return true 	}
+	}
+	return "Usuario y/o Contraseña incorrectos!"
+	// return false
+}
+
 func main() {
-	//For condicional
-	for i := 0; i <= 10; i++ {
-		fmt.Println(i)
+	valor1 := 1
+	valor2 := 2
+
+	if valor1 == 1 {
+		fmt.Println("Es 1")
+	} else {
+		fmt.Println("No es 1")
 	}
 
-	//For while
-	counter := 0
-	for counter < 10 {
-		fmt.Println(counter)
-		counter++
+	//With AND
+	if valor1 == 1 && valor2 == 2 {
+		fmt.Println("Es verdad")
 	}
 
-	//For forever
-	counterForever := 0
-	for {
-		fmt.Println(counterForever)
-		counterForever++
+	//With OR
+	if valor1 == 0 || valor2 == 2 {
+		fmt.Println("Es verdad, OR")
 	}
+
+	//hacer funcion que diga si el numero es par o impar
+	fmt.Println(esPar(13))
+	fmt.Println(esPar(2))
+
+	//hacer funcion de login que retorne true o false
+	fmt.Println(loginAdmin("Admin", "AdminPass"))
 }
