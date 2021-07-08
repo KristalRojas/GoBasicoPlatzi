e4b3de1fd4ffa4828807ba491d16aed1ce3b27c2
krojas <kristal.rojas@mercadolibre.cl>
2021-07-08 14:51:34 -0400

    Keywords Defer, Continue, Break

--- src/main.go
+++ src/main.go
@@ -3,21 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	switch modulo := 4 % 2; modulo {
-	case 0:
-		fmt.Println("Es Par")
-	default:
-		fmt.Println("Es impar")
-	}
+	//Defer
+	//Defer es el keyword que va a ejecutar la ultima funcion antes de que todo muera (referido al main o
+	// equivalente), sirve para usar close.Connection en bd, la buena practica es no mas de un defer por funcion
+	defer fmt.Println("Hola")
+	fmt.Println("Mundo")
 
-	//Switch case sin condicion
-	value := 200
-	switch {
-	case value > 100:
-		fmt.Println("Es mayor a 100")
-	case value < 0:
-		fmt.Println("Es menor a 0")
-	default:
-		fmt.Println("No condicion")
+	//Continue y Break
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		//Continue
+		//Cuando usarlo? es para cuando quieras que siga el for
+		if i == 2 {
+			fmt.Println("Es 2")
+			continue
+		}
+		//Break
+		//Sirve para romper el ciclo de for
+		if i == 8 {
+			break
+		}
 	}
 }
