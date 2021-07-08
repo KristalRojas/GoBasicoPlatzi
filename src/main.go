29b9558a2a0132d93cb8a863d709c2a83ee38fd0
krojas <kristal.rojas@mercadolibre.cl>
2021-07-08 12:26:44 -0400

    Ciclos For

--- src/main.go
+++ src/main.go
@@ -2,29 +2,23 @@ package main
 
 import "fmt"
 
-func normalFunction(message string) {
-	fmt.Println(message)
-}
-
-func tripleArgument(a int, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func returnValue(a int) int {
-	return a * 2
-}
-
-func dobleReturn(a int) (c, d int) {
-	return a, a * 2
-}
-
 func main() {
-	normalFunction("Hola Mundo")
-	tripleArgument(1, 2, "hola")
+	//For condicional
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
 
-	value := returnValue(2)
-	fmt.Println("Value: ", value)
+	//For while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
 
-	value1, value2 := dobleReturn(2)
-	fmt.Println("Value1: ", value1, "  | Value2: ", value2)
+	//For forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 }
