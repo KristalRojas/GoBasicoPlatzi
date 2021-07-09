a78a004dd779695f2b4097af5cb9885b1023a5b0
krojas <kristal.rojas@mercadolibre.cl>
2021-07-08 23:57:56 -0400

    Llave valor con Maps

--- src/main.go
+++ src/main.go
@@ -1,38 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func isPalindromo(text string) {
-	var textReverse string
-	text = strings.ToLower(text)
-
-	for i := len(text) - 1; i >= 0; i-- {
-		textReverse += string(text[i])
-	}
-
-	if text == textReverse {
-		fmt.Println("Es palindromo")
-	} else {
-		fmt.Println("No es palindromo")
-	}
-}
+import "fmt"
 
 func main() {
-	slice := []string{"hola", "que", "hace"}
 
-	//Iterar key value, osea indice y el valor
-	for key, valor := range slice {
-		fmt.Println(key, " | ", valor)
+	//Maps (El map seria una lista de llave valor, en python seria un diccionario)
+	//Creacion de map (make se utiliza para crear distintos tipos de variables)
+	m := make(map[string]int)
+
+	m["Jose"] = 14
+	m["Pepito"] = 20
+
+	fmt.Println(m)
+
+	//Recorrer map
+	for key, value := range m {
+		fmt.Println(key, value)
 	}
 
-	//Iterar solo el valor (Nota: para ignorar alguna variable usar _)
-	for _, valor := range slice {
-		fmt.Println(valor)
-	}
+	//Encontrar valor | retorna 14 que es la key de Jose
+	v := m["Jose"]
+	fmt.Println(v)
 
-	isPalindromo("Amor a roma")
+	//Encontrar valor que no existe | Este retornara el Zero value del tipo que sea el valor
+	//en este caso value es de tipo int, por lo que retornara cero
+	v = m["Josep"]
+	fmt.Println(v)
+
+	//Para saber si se encuentra la item con la key, agregar una variable seguido de la variable que representa
+	//value, esa variable retornara true o false, segun corresponda
+	v, ok := m["Josep"]
+	fmt.Println(v, ok)
 
 }
