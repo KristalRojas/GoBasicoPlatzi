de6110f76dde88a09a7743c293065e23a750b2b6
krojas <kristal.rojas@mercadolibre.cl>
2021-07-08 23:43:01 -0400

    Slices Palindromo

--- src/main.go
+++ src/main.go
@@ -1,33 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindromo(text string) {
+	var textReverse string
+	text = strings.ToLower(text)
+
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
+
+	if text == textReverse {
+		fmt.Println("Es palindromo")
+	} else {
+		fmt.Println("No es palindromo")
+	}
+}
 
 func main() {
-	//Array es inmutable
-	//Array
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array))
+	slice := []string{"hola", "que", "hace"}
 
-	//Slice si puede mutar
-	//Slice
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
+	//Iterar key value, osea indice y el valor
+	for key, valor := range slice {
+		fmt.Println(key, " | ", valor)
+	}
 
-	//Slicing
-	//Metodos en el slice
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
+	//Iterar solo el valor (Nota: para ignorar alguna variable usar _)
+	for _, valor := range slice {
+		fmt.Println(valor)
+	}
 
-	//Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
+	isPalindromo("Amor a roma")
 
-	//Append nueva lista
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
 }
