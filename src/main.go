187dc92fb3e1891d396c69aaf5fb2381a692e655
krojas <kristal.rojas@mercadolibre.cl>
2021-07-12 15:35:25 -0400

    Range, Close y Select

--- src/main.go
+++ src/main.go
@@ -2,22 +2,40 @@ package main
 
 import "fmt"
 
-//Como buena practica, al usar un channel en los parametros de una funcion, se debe indicar si el parametro sera de entrada o de salida
-//Ej: channel sin indicacion: "func say(text string, c chan string) {"
-//Ej: channel indica que sera de entrada: "func say(text string, c chan<- string) {"
-//Ej: channel indica que sera de salida: "func say(text string, c <-chan string) {"
-func say(text string, c chan<- string) {
-	c <- text //c<-, es utiliza para almacenar un valor dentro del channel, este caso almacena el valor de text
+func message(text string, c chan string) {
+	c <- text
 }
 
 func main() {
-	//Creacion de un Channel, es buena practica señalar la cantidad de datos que almacenara el channel
-	//en este caso, el 1 quiere decir que almacenara hasta 1 dato, en caso de no asignar un numero, el valor sera dinamico
-	c := make(chan string, 1)
+	c := make(chan string, 2)
+	c <- "Mensaje1"
+	c <- "Mensaje2"
 
-	fmt.Println("Hello")
+	fmt.Println(len(c), cap(c)) //len indicara la cantidad de datos que tiene, cap indicar la capacidad maxima de channel
 
-	go say("Bye", c) //recordar que go, indica goroutine
+	//Close, es una buena practica cerrar el channel cuando llegue a la capacidad maxima
+	//Es necesario cerrar el channel para recorrerlo, asi ya no espera ningun dato
+	close(c)
 
-	fmt.Println(<-c) //Al usar <-c, se esta extrayendo el valor guardado del channel
+	// c <- "Mensaje3"
+
+	//Range (Recorrido)
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	//Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("Mensaje1", email1)
+	go message("Mensaje2", email2)
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1: ", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email1: ", m2)
+		}
+	}
 }
